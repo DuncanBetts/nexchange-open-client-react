5a9ec121a39b4da194f4adb7ccdf36ad26622120
Duncan Betts <betts.duncan@tuta.io>
2019-11-08 14:56:13 +0000

    Make video responsive, remove unnecessary package.

--- src/components/WhiteLabel/VideoCard/index.js
+++ src/components/WhiteLabel/VideoCard/index.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import { Col, Button } from 'reactstrap'
-import YouTube from 'react-youtube'
+import styled from '@emotion/styled'
 import styles from './VideoCard.scss'
 import { I18n } from 'react-i18next'
 
 
+const IframeContainer = styled.div`
+  position: relative;
+  padding-top: 56.25%;
+  overflow: hidden;
+  height: 0;
+  max-width: 100%;
+`
+
+const Iframe = styled.iframe`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
+
+const videoID = "7ujmzb3HzCA"
+
+const YTPlayer = () => {
+  return (
+  <IframeContainer>
+    <Iframe src={ `https://www.youtube.com/embed/${videoID}` } frameBorder="0" title="Set up your own cryptocurrency exchange" />
+  </IframeContainer>
+  )
+}
+
 export default function VideoCard() {
-  const youtubeOptions = {
-    width: '480px',
-    height: '270px'
-  }
   return (
     <I18n ns="translations">
       {t => (
@@ -21,7 +43,7 @@ export default function VideoCard() {
               <Button>{ t('videocard.contactus') }</Button>
             </Col>
             <Col md="6">
-              <YouTube videoId="7ujmzb3HzCA" opts={youtubeOptions} />
+              <YTPlayer />
             </Col>
           </div>
         </div>
