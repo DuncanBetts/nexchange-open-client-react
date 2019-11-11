438c9a89ae9f3290003a3f70df21a0307e7ef069
Duncan Betts <betts.duncan@tuta.io>
2019-11-11 14:02:44 +0000

    WIP, responsive layout across devices.

--- src/components/WhiteLabel/VideoCard/index.js
+++ src/components/WhiteLabel/VideoCard/index.js
@@ -1,23 +1,47 @@
 import React from 'react'
-import { Col, Button } from 'reactstrap'
+import { Button, Col } from 'reactstrap'
 import styled from '@emotion/styled'
-import styles from './VideoCard.scss'
 import { I18n } from 'react-i18next'
+import ellipse from 'Img/ellipse.svg'
 
+
+
+const PageTitle = styled.h1`
+  grid-column: 1;
+  grid-row: 1;
+  font-size: 3.25rem;
+  text-align: center;
+  @media(min-width: 768px) {
+    font-size: 4rem;
+    text-align: inherit;
+  }
+  @media(min-width: 1536px) {
+    font-size: 5rem;
+  }
+  @media(min-width: 2048px) {
+    font-size: 7rem;
+  }
+`
 
 const IframeContainer = styled.div`
+  grid-row: 2;
+  grid-column: 1;
   position: relative;
   padding-top: 56.25%;
   overflow: hidden;
-  height: 0;
   max-width: 100%;
+  @media(min-width: 768px) {
+    grid-column: 2;
+    grid-row: 1 / 3;
+    align-self: start;
+  }
 `
 
 const Iframe = styled.iframe`
   position: absolute;
-  top: 0;
+  top: 2rem;
   left: 0;
-  width: 100%;
+  width: 97%;
   height: 100%;
 `
 
@@ -31,22 +55,91 @@ const YTPlayer = () => {
   )
 }
 
+// WIP
+const reorder = `
+:nth-child(2) { order: 3 }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  text-align: left;
+  background-color: #01031e;
+  background-image: url( ${ellipse} );
+  background-position: 50% 50%;
+  background-size: 80%;
+  background-repeat: no-repeat;
+  color: #FFF;
+  }
+`
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-rows: 150px 200px 150px;
+  grid-template-columns: 360px;
+  grid-gap: .5rem;
+  @media(min-width: 768px) {
+    grid-template-rows: 250px 125px;
+    grid-template-columns: 348px 398px;;
+  }
+  @media(min-width: 1024px) {
+    grid-template-columns: 15px minmax(497px, 625px) minmax(497px, 625px) 15px;
+  }
+`
+
+// BS3 Container
+// const Container = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: center;
+//   align-items: center;
+//   margin-right: auto;
+//   margin-left: auto;
+//   padding-left: 15px;
+//   padding-right: 15px;
+//   @media (min-width: 768px) {
+//     width: 750px;
+//   }
+//   @media (min-width: 992px) {
+//     width: 970px;
+//   }
+//   @media (min-width: 1200px) {
+//     width: 1170px;
+//   }
+// `
+
+const Buttons = styled.div`
+  padding-top: 3rem;
+  grid-column: 1;
+  grid-row: 3;
+  justify-self: center;
+  @media(min-width: 768px) {
+    padding-top: inherit;
+    grid-row: 2;
+    justify-self: left;
+  }
+`
+
+const StyledButton = styled(Button)`
+  margin: 1rem;
+`
+
 export default function VideoCard() {
   return (
     <I18n ns="translations">
       {t => (
-        <div className={styles.videoContainer}>
-          <div className='container'>
-            <Col md="6">
-              <h1>{ t('videocard.title') }</h1>
-              <Button>{ t('videocard.livepreview') }</Button>
-              <Button>{ t('videocard.contactus') }</Button>
-            </Col>
-            <Col md="6">
-              <YTPlayer />
-            </Col>
-          </div>
-        </div>
+        <Wrapper>
+          <GridContainer>
+            <PageTitle>{ t('videocard.title') }</PageTitle>
+            <Buttons>
+              <StyledButton>{ t('videocard.livepreview') }</StyledButton>
+              <StyledButton>{ t('videocard.contactus') }</StyledButton>
+            </Buttons>
+            <YTPlayer />
+          </GridContainer>
+        </Wrapper>
       )}
     </I18n>
   )
